1943c0fcbdfbe50ea1fcc92234c1077596693425
tiago-a-ribeiro <tiagoribeiro64625@gmail.com>
2021-10-11 18:33:48 -0400

    Update Homework1.m

--- Homework1.m
+++ Homework1.m
@@ -3,14 +3,11 @@ grayimage = rgb2gray(myimage);
 imshow(grayimage, []);
 
 % Problem 1
-logtransform = log(1 + im2double(myimage));
-invlogtransform = exp(im2double(myimage)) - 1; 
+logtransform = log(1 + im2double(myimage)); 
 figure("Name", "Log Transformation");
 imshow(logtransform);
-figure("Name", "Inverse Log Transformation");
-imshow(invlogtransform);
 figure("Name", "Power Law Transformation");
-powerlawtransform = im2double(grayimage).^2;
+powerlawtransform = im2double(myimage).^2;
 imshow(powerlawtransform);
 
 gammapoint3 = im2double(grayimage).^0.3;
@@ -21,11 +18,31 @@ figure("Name", "Gamma = 3");
 imshow(gamma3);
 
 % Problem 2
-
-bitplane8 = bitget(grayimage, 8);
-bitplane7 = bitget(grayimage, 7);
-bitplane6 = bitget(grayimage, 6);
+bitplane1 = bitget(grayimage, 1);
+bitplane2 = bitget(grayimage, 2);
+bitplane3 = bitget(grayimage, 3);
+bitplane4 = bitget(grayimage, 4);
 bitplane5 = bitget(grayimage, 5);
+bitplane6 = bitget(grayimage, 6);
+bitplane7 = bitget(grayimage, 7);
+bitplane8 = bitget(grayimage, 8);
+figure("Name", "Bit plane 1");
+imshow(bitplane1, []);
+figure("Name", "Bit plane 2");
+imshow(bitplane2, []);
+figure("Name", "Bit plane 3");
+imshow(bitplane3, []);
+figure("Name", "Bit plane 4");
+imshow(bitplane4, []);
+figure("Name", "Bit plane 5");
+imshow(bitplane5, []);
+figure("Name", "Bit plane 6");
+imshow(bitplane6, []);
+figure("Name", "Bit plane 7");
+imshow(bitplane7, []);
+figure("Name", "Bit plane 8");
+imshow(bitplane8, []);
+
 % Highest four bits
 highest4bits = bitplane8*(2^8) + bitplane7*(2^7) + bitplane6*(2^6) + bitplane5*(2^5);
 highest2bits = bitplane8*(2^8) + bitplane7*(2^7);
@@ -62,17 +79,13 @@ imshow(EH3);
 
 % Problem 5
 figure("Name", "3-bit Histogram");
-histogram( ...
-    [1,2,4,7,3, ...
-     2,4,7,3,1, ...
-     5,6,2,1,1, ...
-     4,7,1,1,1] ...
-);
+x=[0,1,2,3,4,5,6,7];
+y=[0, 0.35, 0.15, 0.1, 0.15, 0.05, 0.05, 0.15];
+xticks([0 1 2 3 4 5 6 7]);
+plot(x,y, 'o');
 
 figure("Name", "3-bit Histogram Equalized")
-histogram( ...
-    [2,4,5,7,4, ...
-     4,5,7,4,2, ...
-     6,6,4,2,2, ...
-     5,7,2,2,2] ...
-);
+x=[0,1,2,3,4,5,6,7];
+y=[0, 0, 0.35, 0, 0.25, 0.15, 0.1, 0.15];
+xticks([0 1 2 3 4 5 6 7]);
+plot(x,y, 'o');
